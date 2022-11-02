20afb0934674b1b312fe5a9b70fc0affa7e940a6
UTS_muhammadwahyudi <mhd.wahyudi004@mhs.mdp.ac.id>
2022-11-02 11:22:27 +0700

    uts_muhammadawahyudi
    UTS

---
+++ app/src/main/java/uts/muhammadwahyudi/if5b/uts/MainActivity.java
@@ -0,0 +1,16 @@
+package uts.muhammadwahyudi.if5b.uts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et_NL,et_NP,
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/uts/muhammadwahyudi/if5b/uts/uts.java
@@ -0,0 +1,14 @@
+package uts.muhammadwahyudi.if5b.uts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class uts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uts);
+    }
+}
